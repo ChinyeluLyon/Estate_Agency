16b1bce8364849a9b86e5398afba6bfb31d5d452
Chinyelu <chinyelulyon@gmail.com>
2020-10-21 16:42:39 +0100

    can now move pieces on the board to and from locations within their steps boundary

--- estate-agency-MERN-app/frontend/src/components/gameAttempt.component.js
+++ estate-agency-MERN-app/frontend/src/components/gameAttempt.component.js
@@ -19,7 +19,7 @@ export default class TestGame extends React.Component {
             gridHeight: gridHeight,
             grid: gridArr,
             mode: 'place',
-            classSelection: Array(gridArea).fill('gridBox'),
+            classArray: Array(gridArea).fill('gridBox'),
         }
     }
 
@@ -30,20 +30,70 @@ export default class TestGame extends React.Component {
         switch (this.state.mode) {
             case "place":
                 console.log("place mode")
-                let temparr = this.state.grid
-                temparr[boxNum] = 'X'
+                let tempArr = this.state.grid
+                tempArr[boxNum] = 'X'
                 this.setState({
-                    grid: temparr
+                    grid: tempArr
                 })
                 break;
             case "move":
                 console.log("move mode")
-                this.displayRaduis(boxNum)
+                if (this.state.grid[boxNum] === "X") {
+                    this.displayPath(boxNum)
+                }
+                else if (this.state.classArray[boxNum] === "path") {
+                    this.detectMovement(boxNum)
+                }
+                else {
+                    // clear the path
+                    let selectionArr = Array(this.state.gridHeight * this.state.gridWidth).fill('gridBox')
+                    this.setState({
+                        classArray: selectionArr
+                    })
+                }
                 break;
 
             default:
                 break;
         }
+    }
+
+
+    displayPath = (boxNum) => {
+        let steps = 2;
+
+        boxNum = parseInt(boxNum)
+        // let rowSelectedBox = Math.floor(boxNum / 5)
+        // let columnSelectedBox = boxNum - (rowSelectedBox * 5)
+        let tempClassNameArr = Array(this.state.gridHeight * this.state.gridWidth).fill('gridBox')
+        tempClassNameArr[boxNum] = "selectedPiece"
+        for (let i = 0; i < steps; i++) {
+            let up = boxNum - (this.state.gridWidth * (i + 1))
+            let down = (boxNum + (this.state.gridWidth * (i + 1)))
+            let left = boxNum - (i + 1)
+            let right = boxNum + (i + 1)
+
+            tempClassNameArr[up] = 'path'
+            tempClassNameArr[down] = 'path'
+            tempClassNameArr[left] = 'path'
+            tempClassNameArr[right] = 'path'
+            this.setState({
+                classArray: tempClassNameArr
+            })
+        }
+    }
+
+    detectMovement = (boxNum) => {
+        let pieceStartPos = this.state.classArray.indexOf("selectedPiece")
+        let pieceEndPos = boxNum
+        let tempGrid = this.state.grid
+        tempGrid[pieceStartPos] = ""
+        tempGrid[pieceEndPos] = "X"
+
+        this.setState({
+            grid: tempGrid,
+            classArray: Array(this.state.gridHeight * this.state.gridWidth).fill('gridBox')
+        })
 
 
     }
@@ -80,7 +130,7 @@ export default class TestGame extends React.Component {
                 count++
                 xArr.push(
                     <button
-                        className={this.state.classSelection[count - 1]}
+                        className={this.state.classArray[count - 1]}
                         key={count - 1}
                         onMouseDown={this.mouseDownBox}
                         onMouseUp={this.mouseUpBox}
@@ -102,30 +152,6 @@ export default class TestGame extends React.Component {
         return yArr
     }
 
-    displayRaduis = (boxNum) => {
-        let steps = 2;
-
-        boxNum = parseInt(boxNum)
-        // let rowSelectedBox = Math.floor(boxNum / 5)
-        // let columnSelectedBox = boxNum - (rowSelectedBox * 5)
-        let tempClassNameArr = Array(this.state.gridHeight * this.state.gridWidth).fill('gridBox')
-
-        for (let i = 0; i < steps; i++) {
-            let up = boxNum - (this.state.gridWidth * (i + 1))
-            let down = (boxNum + (this.state.gridWidth * (i + 1)))
-            let left = boxNum - (i + 1)
-            let right = boxNum + (i + 1)
-
-            tempClassNameArr[up] = 'selectedBox'
-            tempClassNameArr[down] = 'selectedBox'
-            tempClassNameArr[left] = 'selectedBox'
-            tempClassNameArr[right] = 'selectedBox'
-            this.setState({
-                classSelection: tempClassNameArr
-            })
-        }
-        // selected.props.className = this.state.selectedBox
-    }
 
     render() {
         return (
