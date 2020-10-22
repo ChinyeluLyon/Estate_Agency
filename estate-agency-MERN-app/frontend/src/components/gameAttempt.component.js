9964aaa9072664149be73e8c78ab9e687065cf2f
Chinyelu <chinyelulyon@gmail.com>
2020-10-22 16:46:35 +0100

    can now switch sides, and only place and move my pieces

--- estate-agency-MERN-app/frontend/src/components/gameAttempt.component.js
+++ estate-agency-MERN-app/frontend/src/components/gameAttempt.component.js
@@ -5,8 +5,8 @@ export default class TestGame extends React.Component {
     constructor(props) {
         super(props)
 
-        const gridWidth = 13
-        const gridHeight = 13
+        const gridWidth = 8
+        const gridHeight = 8
         const gridArea = gridWidth * gridHeight
 
         let gridArr = Array(gridArea).fill("")
@@ -20,6 +20,10 @@ export default class TestGame extends React.Component {
             grid: gridArr,
             mode: 'place',
             classArray: Array(gridArea).fill('gridBox'),
+            player1Money: 10,
+            player2Money: 10,
+            currentTurn: 1,
+            currentTurnPiece: "X"
         }
     }
 
@@ -31,14 +35,14 @@ export default class TestGame extends React.Component {
             case "place":
                 console.log("place mode")
                 let tempArr = this.state.grid
-                tempArr[boxNum] = 'X'
+                tempArr[boxNum] = this.state.currentTurnPiece
                 this.setState({
                     grid: tempArr
                 })
                 break;
             case "move":
                 console.log("move mode")
-                if (this.state.grid[boxNum] === "X") {
+                if (this.state.grid[boxNum] === this.state.currentTurnPiece) {
                     this.displayPath(boxNum)
                 }
                 else if (this.state.classArray[boxNum] === "path") {
@@ -80,10 +84,10 @@ export default class TestGame extends React.Component {
         }
         //calc diagonal positions
         let tempBoxNum = boxNum
-        for (let j = 1; j <= steps; j++) {
+        for (let i = 1; i <= steps; i++) {
             tempBoxNum = tempBoxNum - this.state.gridWidth
             let tempBoxNum2 = tempBoxNum
-            for (let k = 0; k < (steps - j); k++) {
+            for (let ii = 0; ii < (steps - i); ii++) {
                 tempBoxNum2++
                 tempClassNameArr[tempBoxNum2] = "path"
             }
@@ -92,72 +96,53 @@ export default class TestGame extends React.Component {
         for (let j = 1; j <= steps; j++) {
             tempBoxNum3 = tempBoxNum3 - this.state.gridWidth
             let tempBoxNum4 = tempBoxNum3
-            for (let k = 0; k < (steps - j); k++) {
+            for (let ji = 0; ji < (steps - j); ji++) {
                 tempBoxNum4--
                 tempClassNameArr[tempBoxNum4] = "path"
             }
         }
         let tempBoxNum5 = boxNum
-        for (let j = 1; j <= steps; j++) {
+        for (let k = 1; k <= steps; k++) {
             tempBoxNum5 = tempBoxNum5 + this.state.gridWidth
             let tempBoxNum6 = tempBoxNum5
-            for (let k = 0; k < (steps - j); k++) {
+            for (let ki = 0; ki < (steps - k); ki++) {
                 tempBoxNum6++
                 tempClassNameArr[tempBoxNum6] = "path"
             }
         }
         let tempBoxNum7 = boxNum
-        for (let j = 1; j <= steps; j++) {
+        for (let l = 1; l <= steps; l++) {
             tempBoxNum7 = tempBoxNum7 + this.state.gridWidth
             let tempBoxNum8 = tempBoxNum7
-            for (let k = 0; k < (steps - j); k++) {
+            for (let li = 0; li < (steps - l); li++) {
                 tempBoxNum8--
                 tempClassNameArr[tempBoxNum8] = "path"
             }
         }
 
-        //calc bounds proper
+        //calc bounds 
         let row = Math.floor(boxNum / this.state.gridWidth)
         let column = boxNum - (row * this.state.gridWidth)
         let spaceRight = this.state.gridWidth - column - 1
-        console.log("row " + row)
-        console.log("column " + column)
-        console.log("space left " + column)
-        console.log("space right " + (this.state.gridWidth - column - 1))
-        if (column >= steps && spaceRight >= steps) {
-            console.log("fits fine")
-        }
-        else if (column < steps) {
-            console.log("not enough on left")
-            console.log("columns 0 to " + (column + steps))
+
+        if (column < steps) {
             let remainingRight = this.state.gridWidth - (column + steps)
             for (let iterator = 0; iterator < this.state.gridHeight; iterator++) {
                 let limitR = (this.state.gridWidth * iterator) + (column + steps + 1)
-                console.log("limitR " + limitR)
                 for (let index = 0; index < remainingRight - 1; index++) {
-                    console.log("dbg: " + (limitR + index))
                     tempClassNameArr[limitR + index] = "gridBox"
                 }
             }
         }
         else if (spaceRight < steps) {
-            console.log("not enough on right")
-            console.log("columns " + (column - steps) + " to " + (this.state.gridWidth - 1))
             let remainingLeft = (column - steps - 1)
-            console.log("remainingLeft " + remainingLeft)
             for (let i = 0; i < this.state.gridHeight; i++) {
-                console.log("startL: " + (i * this.state.gridWidth))
                 let startL = (i * this.state.gridWidth)
                 for (let j = 0; j < (remainingLeft + 1); j++) {
-                    console.log(startL + j)
                     tempClassNameArr[startL + j] = "gridBox"
                 }
             }
         }
-
-
-
-
 
         this.setState({
             classArray: tempClassNameArr
@@ -169,26 +154,13 @@ export default class TestGame extends React.Component {
         let pieceEndPos = boxNum
         let tempGrid = this.state.grid
         tempGrid[pieceStartPos] = ""
-        tempGrid[pieceEndPos] = "X"
+        tempGrid[pieceEndPos] = this.state.currentTurnPiece
 
         this.setState({
             grid: tempGrid,
             classArray: Array(this.state.gridHeight * this.state.gridWidth).fill('gridBox')
         })
-
-
     }
-
-    mouseDownBox = (event) => {
-        console.log("clicked on")
-        // event.target.className = this.state.selectedBox
-        // console.log(event)
-    }
-    mouseUpBox = (event) => {
-        console.log("clicked off")
-        // event.target.className = this.state.unselectedBox
-    }
-
 
     clickPlace = () => {
         this.setState({
@@ -215,7 +187,21 @@ export default class TestGame extends React.Component {
                 mode: "move"
             })
         }
+    }
 
+    switchTurn = () => {
+        if (this.state.currentTurn === 1) {
+            this.setState({
+                currentTurn: 2,
+                currentTurnPiece: "O"
+            })
+        }
+        else {
+            this.setState({
+                currentTurn: 1,
+                currentTurnPiece: "X"
+            })
+        }
     }
 
     createGrid = () => {
@@ -229,8 +215,6 @@ export default class TestGame extends React.Component {
                     <button
                         className={this.state.classArray[count - 1]}
                         key={count - 1}
-                        onMouseDown={this.mouseDownBox}
-                        onMouseUp={this.mouseUpBox}
                         onClick={this.clickBox}
                         onContextMenu={this.rightClick}
                         value={count - 1}
@@ -255,9 +239,13 @@ export default class TestGame extends React.Component {
     render() {
         return (
             <div>
-                <h1>Game Attempt</h1>
+                <h1>Game</h1>
+                <h2>It's Player {this.state.currentTurn}'s turn</h2>
+                <h2>Player 1: £{this.state.player1Money}</h2>
+                <h2>Player 2: £{this.state.player2Money}</h2>
                 <button onClick={this.clickPlace}>Place</button>
                 <button onClick={this.clickMove}>Move</button>
+                <button onClick={this.switchTurn}>End Turn</button>
                 <br />
                 <div className="wholeGame">
                     {this.createGrid()}
